ae71c94b6fb922f356218289cb756ff3af0a5822
blazej-smorawski <blazej.smorawski@gmail.com>
2023-01-17 13:07:42 +0100

    Improve `entrypoint.sh`

--- README.md
+++ README.md
@@ -14,10 +14,10 @@ so we'd like to dispatch user code into docker containers and run it in a
 safe and consistent environment. Also, we want to keep it as simple as possible.
 
 ## Running in container
-In order to run `lynx-runner` at port `9233` use:
+In order to run `lynx-runner:0.4` with `lynx-runtime:0.3` at port `9233` use:
 
-    podman pull ghcr.io/group-project-gut/lynx-runner:0.1
-    podman run --privileged -p 9233:9000 lynx-runner:0.1
+    podman pull ghcr.io/group-project-gut/lynx-runner:0.4
+    podman run --privileged -p 9233:9233 lynx-runner:0.4 0.3 9233
 
 ## Development
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-podman pull ghcr.io/group-project-gut/lynx-runtime:0.3
+# Colors for printing
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
 
-./target/release/lynx-runner 9000
+# Pull runtime image
+PULL_CMD="podman pull ghcr.io/group-project-gut/lynx-runtime:$1"
+PULL_OUT=$($PULL_CMD)
+if [ $? == 0 ]; then
+    echo -e "${GREEN} [OK]\t ${NC} Succesfully pulled runtime:$1 image from ghcr"
+else
+    echo -e "${RED} [ERROR]\t\t ${NC} Failed to pull runtime:$1 image from ghcr"
+    echo $PULL_OUT
+    exit 1
+fi
+
+# Run runner exec
+echo -e "${GREEN} [OK]\t ${NC} Running runnner:0.4 at port $2"
+./target/release/lynx-runner $2
